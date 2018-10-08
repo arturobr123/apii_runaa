9c82abf40d379675e5003feb79d6648e6daffac8
Arturo Bravo Rovirosa <arturo123leyenda@gmail.com>
2018-10-08 10:43:38 -0500

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   validates_length_of       :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
   validates_confirmation_of :password, allow_nil: true, allow_blank: false
 
+  before_validation {
+    (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase)
+  }
 
   # Make sure email and username are present and unique.
   validates_presence_of     :email
